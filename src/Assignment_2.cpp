d8e125ccef5656d24ec8c44d5233597cf18c6cfb
Hugh Al <Hugh.Al@outlook.com>
2017-05-01 22:19:24 +1000

    "Changes to main"

--- src/Assignment_2.cpp
+++ src/Assignment_2.cpp
@@ -7,9 +7,17 @@
 //============================================================================
 
 #include <iostream>
+#include "Idea.h"
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	vector<string> keyword;
+	keyword.push_back("computer");
+	keyword.push_back("algorithm");
+
+
+	Idea test("Huy", keyword, "blahblah");
+	test.toString();
+
 	return 0;
 }
